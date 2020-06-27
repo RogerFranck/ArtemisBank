4789d3cdb715909d94c5882c7883dc4880f6b19f
Roger <RogerAlmeydaRamos@outlook.com>
2020-06-26 21:28:04 -0500

    tostilocos

--- frontend/src/Pages/User/Depositos.js
+++ frontend/src/Pages/User/Depositos.js
@@ -9,8 +9,46 @@ import '../../assets/Depositos.css'
 import FormControl from '@material-ui/core/FormControl';
 import Select from '@material-ui/core/Select';
 import CardHeader from '../../Components/CardHeader'
+import axios from 'axios';
 
 export default class Depositos extends Component {
+  constructor() {
+    super();
+    this.state = {
+      user: '',
+      cantidad: 0,
+      dinero: 0,
+    };
+  }
+
+  componentDidMount = async () => {
+    const jwt = localStorage.getItem('JWT-COOL');
+    if (jwt) {
+      const user = await axios.get('http://localhost:4000/login/validar', {
+        headers: {
+          "x-jwt": jwt
+        }
+      });
+      this.setState({
+        user:user.data._id
+      });
+    }
+    else {
+      window.location.href = "/login";
+    }
+  }
+
+  onSubmit = async (e) => {
+    e.preventDefault();
+
+    const usersi = await axios.post('http://localhost:4000/api/deposito/user/' + this.state.user, {
+      dinero: this.state.dinero,
+      cantidad: this.state.cantidad,
+    });
+
+    alert("depositado");
+
+  }
   render() {
     return (
       <Grid container style={{ minHeight: "100vh" }} justify="center" alignItems="center" >
@@ -18,38 +56,53 @@ export default class Depositos extends Component {
           <CardHeader mensaje="Depositar a mi cuenta" />
           <CardContent>
             <div style={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
-
-
-              <FormControl variant="outlined" style={{ width:150}} >
-                <Select
-                  native
-                  //value={state.age}
-                  //onChange={handleChange}
-                  inputProps={{
-                    name: 'age',
-                    id: 'age-native-simple',
+              <form id="fomr1" onSubmit={this.onSubmit}>
+                <FormControl variant="outlined" style={{ width: 150 }} >
+                  <Select
+                    native
+                    value={this.state.dinero}
+                    onChange={(e) => {
+                      const { name, value } = e.target;
+                      this.setState({
+                        [name]: value,
+                      });
+                    }}
+                    name="dinero"
+                    inputProps={{
+                      name: 'dinero',
+                    }}
+                  >
+                    <option aria-label="None" value="" />
+                    <option value={500}>$500</option>
+                    <option value={200}>$200</option>
+                    <option value={100}>$100</option>
+                    <option value={50}>$50</option>
+                    <option value={20}>$20</option>
+                    <option value={10}>$10</option>
+                    <option value={5}>$5</option>
+                    <option value={2}>$2</option>
+                    <option value={1}>$1</option>
+                  </Select>
+                </FormControl>
+                <TextField
+                  label="Cantidad"
+                  variant="outlined"
+                  type="number"
+                  value={this.state.cantidad}
+                  onChange={(e) => {
+                    const { name, value } = e.target;
+                    this.setState({
+                      [name]: value,
+                    });
                   }}
-                >
-                  <option aria-label="None" value="" />
-                  <option value={500}>$500</option>
-                  <option value={200}>$200</option>
-                  <option value={100}>$100</option>
-                  <option value={50}>$50</option>
-                  <option value={20}>$20</option>
-                  <option value={10}>$10</option>
-                  <option value={5}>$5</option>
-                  <option value={2}>$2</option>
-                  <option value={1}>$1</option>
-                </Select>
-              </FormControl>
-
-
-              <TextField label="Cantidad" variant="outlined" type="number" />
+                  name="cantidad"
+                />
+              </form>
             </div>
           </CardContent>
           <CardActions style={{ display: "flex", justifyContent: "flex-end" }}>
             <Button onClick={() => window.location.href = "/"} color="primary">Regresar</Button>
-            <Button color="primary">Depositar</Button>
+            <Button color="primary" type="submit" form="fomr1" >Depositar</Button>
           </CardActions>
         </Card>
       </Grid>
